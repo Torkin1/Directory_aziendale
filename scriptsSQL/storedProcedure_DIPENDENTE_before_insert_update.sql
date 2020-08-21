3463b726d2789c1cf305e35f91e985d3a31040e4
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-08-21 16:52:01 +0200

    #57

--- scriptsSQL/storedProcedure_DIPENDENTE_before_insert_update.sql
+++ scriptsSQL/storedProcedure_DIPENDENTE_before_insert_update.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DIPENDENTE_before_insert_update`(in email varchar(45), in numTelefonicoEsternoPostazione varchar(45), in cfDipendente char(16), in dataUltimoTrasferimento date)
 BEGIN
 	call checkEmail(email);
-	if not (select NumTelefonicoEsternoPostazione from TRASFERITO_A where TRASFERITO_A.NumTelefonicoEsternoPostazione = numTelefonicoEsternoPostazione and TRASFERITO_A.`Data` = dataUltimoTrasferimento)
+	if (select NumTelefonicoEsternoPostazione from TRASFERITO_A where TRASFERITO_A.NumTelefonicoEsternoPostazione = numTelefonicoEsternoPostazione and TRASFERITO_A.`Data` = dataUltimoTrasferimento) is null
         then signal sqlstate '45003'
         set message_text =  "ERROR: Employer was not registered as TRASFERITO_A to designed postation.";
     end if;
--- scriptsSQL/trigger_trasferito_a_before_insert.sql
+++ scriptsSQL/trigger_trasferito_a_before_insert.sql
@@ -4,14 +4,13 @@ TRIGGER `directory_aziendale`.`TRASFERITO_A_BEFORE_INSERT`
 BEFORE INSERT ON `directory_aziendale`.`TRASFERITO_A`
 FOR EACH ROW
 BEGIN
-	if not (select CFDipendente from DA_TRASFERIRE_A where new.CFDipendente = DA_TRASFERIRE_A.CFDipendente)
-		then signal sqlstate '45002' set message_text = "ERROR: Non è possibile registare un dipendente come trasferito se prima non è stato contrassegnato come da trasferire."; 
-	elseif (select NumTelefonicoEsternoPostazione, `Data` 
+	
+	if (select NumTelefonicoEsternoPostazione, `Data` 
 			from TRASFERITO_A 
 			where TRASFERITO_A.NumTelefonicoEsternoPostazione = new.NumTelefonicoEsternoPostazione 
 				and TRASFERITO_A.CFDipendente = new.CFDipendente
                 and timestampdiff(year, TRASFERITO_A.`Data`, curdate()) <= 3 
-			)
+			) is not null
 		then signal sqlstate '45004' set message_text = "ERROR: Un dipendente non può essere trasferito a una postazione dove è stato già trasferito meno di tre anni fa";
     else
 		delete from DA_TRASFERIRE_A where new.CFDipendente = DA_TRASFERIRE_A.CFDipendente;
