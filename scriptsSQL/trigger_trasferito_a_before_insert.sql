4704a52ed2985bc05c580d24c064df0dc6f39aa8
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-08-21 13:49:25 +0200

    #52

--- scriptsSQL/trigger_trasferito_a_before_insert.sql
+++ scriptsSQL/trigger_trasferito_a_before_insert.sql
@@ -1,10 +1,19 @@
--- Controlla che esista una tupla in DA_TRASFERIRE_A che corrisponda a quella che si vuole inserire in TRASFERITO_A. Se c'è, la cancella, altrimenti abortisce l'operazione
-
-CREATE DEFINER = CURRENT_USER TRIGGER `directory_aziendale`.`TRASFERITO_A_BEFORE_INSERT` BEFORE INSERT ON `TRASFERITO_A` FOR EACH ROW
+CREATE
+DEFINER=`root`@`localhost`
+TRIGGER `directory_aziendale`.`TRASFERITO_A_BEFORE_INSERT`
+BEFORE INSERT ON `directory_aziendale`.`TRASFERITO_A`
+FOR EACH ROW
 BEGIN
-	if (select CFDipendente from DA_TRASFERIRE_A where new.CFDipendente = DA_TRASFERIRE_A.CFDipendente)
-		then delete from DA_TRASFERIRE_A where new.CFDipendente = DA_TRASFERIRE_A.CFDipendente;
-	else
-		signal sqlstate '45002' set message_text = "ERROR: Non è possibile registare un dipendente come trasferito se prima non è stato contrassegnato come da trasferire.";
+	if not (select CFDipendente from DA_TRASFERIRE_A where new.CFDipendente = DA_TRASFERIRE_A.CFDipendente)
+		then signal sqlstate '45002' set message_text = "ERROR: Non è possibile registare un dipendente come trasferito se prima non è stato contrassegnato come da trasferire."; 
+	elseif (select NumTelefonicoEsternoPostazione, `Data` 
+			from TRASFERITO_A 
+			where TRASFERITO_A.NumTelefonicoEsternoPostazione = new.NumTelefonicoEsternoPostazione 
+				and TRASFERITO_A.CFDipendente = new.CFDipendente
+                and timestampdiff(year, TRASFERITO_A.`Data`, curdate()) <= 3 
+			)
+		then signal sqlstate '45004' set message_text = "ERROR: Un dipendente non può essere trasferito a una postazione dove è stato già trasferito meno di tre anni fa";
+    else
+		delete from DA_TRASFERIRE_A where new.CFDipendente = DA_TRASFERIRE_A.CFDipendente;
     end if;
 END
