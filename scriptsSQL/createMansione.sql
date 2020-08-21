619c94cd68c204ece4040e3caacda2d2cc8bfa0a
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-08-21 14:56:40 +0200

    #54

--- scriptsSQL/createMansione.sql
+++ scriptsSQL/createMansione.sql
@@ -1,12 +1,7 @@
-drop table MANSIONE;
 create table if not exists `directory_aziendale`.`MANSIONE`(
 	Nome varchar(45),
-    NomeSettore varchar(45),
-    EmailUfficio varchar(45),
+    NomeSettore varchar(45) unique,
+    EmailUfficio varchar(45) unique,
     primary key (Nome, NomeSettore)
 )
 comment = 'Registra le mansioni svolte all\'interno dell\'azienda. Esse sono identificate dal loro nome e dal nome del Settore cui fanno parte, in maniera tale che ogni settore possa nominare le sue mansione l\'uno in maniera indipendente dall\'altro';
-alter table directory_aziendale.MANSIONE
-    add constraint NomeSettore_UNIQUE unique (NomeSettore),
-    add constraint EmailUfficio_UNIQUE unique (EmailUfficio)
-    
--- scriptsSQL/view_DipendentiDaTrasferirePeriodoScadutoConMansione.sql
+++ scriptsSQL/view_DipendentiDaTrasferirePeriodoScadutoConMansione.sql
@@ -1,11 +1,21 @@
 -- Elenca tutti i dipendenti di cui il periodo di turnazione è decorso con la loro attuale mansione.
 -- Si assume che il periodo di turnazione dei dipendenti sia di 30 giorni.
 
-CREATE VIEW `dipendentiDaTrasferirePeriodoScadutoConMansione` AS
-	select DIPENDENTE.CF as CFDipendente, UFFICIO_FISICO.NomeMansione, UFFICIO_FISICO.NomeSettore 
-	from DIPENDENTE join POSTAZIONE join UFFICIO_FISICO 
-		on DIPENDENTE.NumTelefonicoEsternoPostazione = POSTAZIONE.NumTelefonicoEsterno 
-		and POSTAZIONE.NumUfficio = UFFICIO_FISICO.Numero
-        and POSTAZIONE.NumPiano = UFFICIO_FISICO.NumPiano
-        and POSTAZIONE.IndirizzoEdificio = UFFICIO_FISICO.IndirizzoEdificio
-    where datediff(DIPENDENTE.DataUltimoTrasferimento, current_date) > 30
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `directory_aziendale`.`dipendentiDaTrasferirePeriodoScadutoConMansione` AS
+    SELECT 
+        `directory_aziendale`.`DIPENDENTE`.`CF` AS `CFDipendente`,
+        `directory_aziendale`.`UFFICIO_FISICO`.`NomeMansione` AS `NomeMansione`,
+        `directory_aziendale`.`UFFICIO_FISICO`.`NomeSettore` AS `NomeSettore`
+    FROM
+        ((`directory_aziendale`.`DIPENDENTE`
+        JOIN `directory_aziendale`.`POSTAZIONE`)
+        JOIN `directory_aziendale`.`UFFICIO_FISICO` ON (((`directory_aziendale`.`DIPENDENTE`.`NumTelefonicoEsternoPostazione` = `directory_aziendale`.`POSTAZIONE`.`NumTelefonicoEsterno`)
+            AND (`directory_aziendale`.`POSTAZIONE`.`NumUfficio` = `directory_aziendale`.`UFFICIO_FISICO`.`Numero`
+            and `directory_aziendale`.`POSTAZIONE`.`NumPiano` = `directory_aziendale`.`UFFICIO_FISICO`.`NumPiano`
+            and `directory_aziendale`.`POSTAZIONE`.`IndirizzoEdificio` = `directory_aziendale`.`UFFICIO_FISICO`.`IndirizzoEdificio`))))
+    WHERE
+        ((TO_DAYS(`directory_aziendale`.`DIPENDENTE`.`DataUltimoTrasferimento`) - TO_DAYS(CURDATE())) > 30)
