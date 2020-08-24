d70b0ba96ad71f2a39e4ea83f1760923cb189232
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-08-24 10:41:22 +0200

    #59

--- scriptsSQL/createTrasferitoA.sql
+++ scriptsSQL/createTrasferitoA.sql
@@ -1,9 +1,9 @@
 create table if not exists directory_aziendale.TRASFERITO_A(
-	CFDipendente char(16),
+	CFDipendente char(16) not null,
     NumTelefonicoEsternoPostazione varchar(45),
     `Data` date,
+    NomeMansione varchar(45) not null,
+    NomeSettore varchar(45) not null,
     primary key (NumTelefonicoEsternoPostazione, `Data`)
 )
 comment = 'Registra tutti i trasferimenti a cui i dipendenti dell\'azienda sono stati sottoposti durante il corso del tempo.';
-alter table directory_aziendale.TRASFERITO_A
-	modify CFDipendente_NN char(16) not null
