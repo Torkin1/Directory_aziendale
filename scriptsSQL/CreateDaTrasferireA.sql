9d8d2e4b3dc5d7c914781654287ed7c47dc497cf
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-08-26 17:02:36 +0200

    Update CreateDaTrasferireA.sql
    file was not in sync with Relazione

--- scriptsSQL/CreateDaTrasferireA.sql
+++ scriptsSQL/CreateDaTrasferireA.sql
@@ -1,8 +1,6 @@
-CREATE TABLE `DA_TRASFERIRE_A` (
-  `CFDipendente` char(16) NOT NULL,
-  `NomeMansione` varchar(45) NOT NULL,
-  `NomeSettore` varchar(45) NOT NULL,
-  PRIMARY KEY (`CFDipendente`),
-  UNIQUE KEY `NomeMansione` (`NomeMansione`,`NomeSettore`),
-  CONSTRAINT `CFDipendente_FK` FOREIGN KEY (`CFDipendente`) REFERENCES `DIPENDENTE` (`CF`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Registra tutti i dipendenti che devono essere sottoposti a trasferimento, specificando la mansione che dovranno svolgere a trasferimento compiuto. Se un dipendente è trasferito a una postazione diversa dello stesso ufficio, oppure a un altro ufficio che svolge la stessa mansione, la mansione registrata sarà quella che il dipendente stava svolgendo prima di essere trasferito.';
+create table if not exists directory_aziendale.DA_TRASFERIRE_A(
+	CFDipendente char(16) primary key,
+    NomeMansione varchar(45) not null,
+    NomeSettore varchar(45) not null
+)
+comment = 'Registra tutti i dipendenti che devono essere sottoposti a trasferimento, specificando la mansione che dovranno svolgere a trasferimento compiuto. Se un dipendente è trasferito a una postazione diversa dello stesso ufficio, oppure a un altro ufficio che svolge la stessa mansione, la mansione registrata sarà quella che il dipendente stava svolgendo prima di essere trasferito.'
