57bcba4726b411371900fac29435cb1d2b8c7c85
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-08-26 16:54:21 +0200

    #46

--- scriptsSQL/event_op8.sql
+++ scriptsSQL/event_op8.sql
@@ -3,4 +3,4 @@
 create event directory_aziendale.trovaUtentiDaTrasferire on schedule every 1 day on completion preserve do
 	insert ignore into DA_TRASFERIRE_A (CFDipendente, NomeMansione, NomeSettore)
     select CFDipendente, NomeMansione, NomeSettore
-    from dipendentiDaTrasferirePeriodoScadutoConMansione;
+    from view_DipendentiDaTrasferirePeriodoScadutoConMansione;
