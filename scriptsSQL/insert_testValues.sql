046023f4e1f33656a2b0fe0ae5dec1e5a191514e
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-09-07 02:42:09 +0200

    #82

--- scriptsSQL/insert_testValues.sql
+++ scriptsSQL/insert_testValues.sql
@@ -6,14 +6,23 @@ insert into directory_aziendale.MANSIONE values ("calcolo bilancio", "contabilit
 insert into directory_aziendale.MANSIONE values ("calcolo stipendi", "contabilità", "ufficioCalcoloStipendi@mail.com");
 insert into directory_aziendale.MANSIONE values ("reclami", "servizio clienti", "ufficoReclami@mail.com");
 
-insert into directory_aziendale.UFFICIO_FISICO values (null, null, 0001, 1, "via degli uffici, 12");
+insert into directory_aziendale.UFFICIO_FISICO values ("calcolo stipendi", "contabilità", "0001", 1, "via degli uffici, 12");
 insert into directory_aziendale.UFFICIO_FISICO values ('a', 'b', "0002", 1, "via degli uffici, 12"); -- must fail for FK
 insert into directory_aziendale.UFFICIO_FISICO values ("calcolo bilancio", "contabilità", "0003", 0, "via dei test, 55");
 
-insert into POSTAZIONE values ("3313747567", "0003-01", "0003", 0, "via dei test, 55");
+insert into POSTAZIONE values ("3313747567", "0003-01", "0003", "0", "via dei test, 55");
 insert into POSTAZIONE values ("1230987654", "0003-02", "0003", "0", "via dei test, 55");
 insert into POSTAZIONE values ("2536485834", "0003-03", "0003", "0", "via dei test, 55");
 insert into POSTAZIONE values ("1234525234", "0003-04", "0003", "0", "via dei test, 55");
+insert into POSTAZIONE values ("2727378237", "0003-05", "0003", "0", "via dei test, 55");
+insert into POSTAZIONE values ("3747238924", "0003-06", "0003", "0", "via dei test, 55");
+insert into POSTAZIONE values ("8582484945", "0003-07", "0003", "0", "via dei test, 55");
+insert into POSTAZIONE values ("7432874987", "0001-01", "0001", 1, "via degli uffici, 12");
+
+insert into TRASFERITO_A values ("LNYRYM72A08Z327C", "8582484945", curdate(), "calcolo bilancio", "contabilità");
+insert into TRASFERITO_A values ("TXHTGD97E65H851W", "3313747567", curdate(), "calcolo bilancio", "contabilità");
+truncate table TRASFERITO_A;
+-- call elencaTrasferimentiDipendente("LNYRYM72A08Z327C");
 
 insert into directory_aziendale.DIPENDENTE values ("TXHTGD97E65H851W", "Paolo", "Bonolis", null, null, null, null, "3313747567", curdate());
 insert into directory_aziendale.DIPENDENTE values ("LNYRYM72A08Z327C", "Luca", "Laurenti", null, null, null, null, null, null);
@@ -24,9 +33,7 @@ insert into directory_aziendale.DIPENDENTE values ("SLYZMS53P67E762A", "Fiammett
 insert into directory_aziendale.DIPENDENTE values ("DFPOOU82M67G482U", "Gigi", "D'Alessio", null, null, null, null, null, null);
 insert into directory_aziendale.DIPENDENTE values ("ZHNBDB29C66F457F", "Paolo", "The Second", null, null, null, null, null, null);
 call cambiaMansioneDipendente("DFPOOU82M67G482U", "reclami", "Servizio Clienti");
-call assumiDipendente("HMFMUO92S01A827B", "Diletta", "Leotta", null, null, null, null, "calcolo stipendi", "contabilità");
-
-insert into directory_aziendale.TRASFERITO_A values ("TXHTGD97E65H851W", "3313747567", curdate());
+-- call assumiDipendente("HMFMUO92S01A827B", "Diletta", "Leotta", null, null, null, null, "calcolo stipendi", "contabilità");
 
 insert into DA_TRASFERIRE_A values ("TXHTGD97E65H851W", "reclami", "servizio clienti");
 insert into DA_TRASFERIRE_A values ("LNYRYM72A08Z327C", "calcolo bilancio", "contabilità");
@@ -34,13 +41,19 @@ insert into DA_TRASFERIRE_A values ("BGNXNS34P02B229F", "calcolo bilancio", "con
 insert into DA_TRASFERIRE_A values ("FQFZPK35T45L146X", "calcolo stipendi", "contabilità");
 insert into DA_TRASFERIRE_A values ("BHZDND52E64F784E", "reclami", "servizio clienti");
 insert into DA_TRASFERIRE_A values ("SLYZMS53P67E762A", "calcolo stipendi", "contabilità");
-call generaReportDaTrasferire();
+-- call generaReportDaTrasferire();
 
-insert into TRASFERITO_A values ("LNYRYM72A08Z327C", "1230987654", curdate(), "calcolo bilancio", "contabilità");
-insert into TRASFERITO_A values ("TXHTGD97E65H851W", "3313747567", curdate(), "calcolo bilancio", "contabilità");
-call elencaTrasferimentiDipendente("LNYRYM72A08Z327C");
 
-call ricercaDipendente("Paolo", null);
-call ricercaPerNumeroTelefono("2536485834");
+
+call ricercaDipendente("Paolo", "Bonolis");
+/*call ricercaPerNumeroTelefono("2536485834");
 call trovaUfficiConPostazioneVuota(null, null);
 call trovaDipendentiScambiabili("ZHNBDB29C66F457F");
+call scambiaDipendenti("TXHTGD97E65H851W", "LNYRYM72A08Z327C");
+call assegnaDipendenteAPostazioneVuota("SLYZMS53P67E762A", "3747238924");
+*/
+call scambiaDipendenti("LNYRYM72A08Z327C","TXHTGD97E65H851W");
+call checkDipendentiStessaMansione("BGNXNS34P02B229F", null, @a, @b);
+-- call getMansioneDaPostazioneSeCorrispondeADaTrasferireA("LNYRYM72A08Z327C", "1230987654", @a, @b);
+select @a, @b;
+truncate TRASFERITO_A;
